b2b5b88e739abdd00199c799fc592d943877cbee
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-04-09 17:33:37 -0700

    Remove kibit from plugins

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/math.combinatorics "0.0.4"]]
-  :plugins [[lein-kibit "0.0.8"]])
+                 [org.clojure/math.combinatorics "0.0.4"]])
