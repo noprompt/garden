7bb4199eaf16c5c7e6225bd4996fb83d11495472
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-04-14 21:46:37 -0700

    Place all media features in a set

--- src/garden/media.clj
+++ src/garden/media.clj
@@ -1,35 +1,27 @@
 (ns garden.media
-  "Utility functions for working with media queries."
-  (:require [garden.util :refer :all]
-            [garden.units :as unit]))
+  "Utility functions for working with media queries.")
 
 ;; See: http://www.w3.org/TR/css3-mediaqueries/#media1
-;; Feature, accepted value type or values, accept min/max prefixes
 (def media-features
-  {:width [unit/length? true]
-   :height [unit/length? true]
-   :device-width [unit/length? true]
-   :device-height [unit/length? true]
-   :orientation [#{:portrait :landscape} false]
-   :aspect-ratio [ratio? true]
-   :device-aspect-ratio [ratio? true]
-   :color [natural? true]
-   :color-index [natural? true]
-   :monochrome [natural? true]
-   :resolution [unit/resolution? true]
-   :scan [#{:progressive :interlace} false]
-   :grid [natural? false]})
-
-(def media-feature-names
-  (letfn [(prefix-keyword [k p]
-            (keyword (as-str  p \-  k)))
-          (extract-names [coll [feature [_ prefix?]]]
-            (if prefix?
-              (->> (map (partial prefix-keyword feature) [:min :max])
-                   (cons feature)
-                   (into coll))
-              (conj coll feature)))]
-    (reduce extract-names #{} media-features)))
-
-(def media-feature-types
-  #{:all :braille :embossed :handheld :print :projection :screen :speech :tty :tv})
+  #{:all
+    :aspect-ratio :min-aspect-ratio :max-aspect-ratio
+    :braille
+    :color :min-color :max-color
+    :color-index :min-color-index :max-color-index
+    :device-height :min-device-height :max-device-height
+    :device-width :min-device-width :max-device-width
+    :embossed
+    :grid
+    :handheld
+    :height :min-height :max-height
+    :monochrome :min-monochrome :max-monochrome
+    :orientation
+    :print
+    :projection
+    :resolution :min-resolution :max-resolution
+    :scan
+    :screen
+    :speeach
+    :tty
+    :tv
+    :width :min-width :max-width})
