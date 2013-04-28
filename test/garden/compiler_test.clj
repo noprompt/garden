8e922c3b11cd263e263dc619e2e0b030a6b8a849
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-04-27 18:45:52 -0700

    Refactor and organize tests
    Compiler tests no longer test `with-output-style` behavior, instead this
    has been moved to the core test suite.
    
    Sample CSS files have been added to make testing the output easier.
    Testing strings was becoming a real headache. The compiler tests should
    be set up this way soon.

--- test/garden/compiler_test.clj
+++ test/garden/compiler_test.clj
@@ -1,69 +1,32 @@
 (ns garden.compiler_test
   (:require [clojure.test :refer :all]
-            [garden.compiler :refer :all]
-            [garden.util :refer [with-output-style]]))
+            [garden.compiler :refer :all]))
 
 (deftest test-with-output-style
-  (let [declaration {:foo "bar"}
-        declaration' {:foo {:bar {:baz "quux"}}}
-        rule [:foo {:bar "baz"} {:quux "grault"}]
-        rule' [:foo {:bar "baz"} [:quux {:corge "grault"}]]
-        rule'' [:foo :bar {:bar "baz"} [:quux :corge {:corge "grault"}]]
-        rule''' [:foo :bar '({:bar "baz"}) '([:quux :corge {:corge "grault"}])]
-        stylesheet [[:foo {:bar "baz"}] [:quux {:corge "grault"}]]
-        stylesheet' (with-meta [:h1 {:foo "bar"}] {:screen true})]
-    (testing "with output :compressed"
-      (with-output-style :compressed
-        (is (= (render-css declaration)
-               "foo:bar"))
-        (is (= (render-css declaration')
-               "foo-bar-baz:quux"))
-        (is (= (render-css rule)
-               "foo{bar:baz;quux:grault}"))
-        (is (= (render-css rule')
-               "foo{bar:baz}foo quux{corge:grault}"))
-        (is (= (render-css rule'')
-               "foo,bar{bar:baz}foo quux,foo corge,bar quux,bar corge{corge:grault}"))
-        (is (= (render-css rule''')
-               "foo,bar{bar:baz}foo quux,foo corge,bar quux,bar corge{corge:grault}"))
-        (is (= (render-css stylesheet)
-               "foo{bar:baz}quux{corge:grault}"))))
-    (testing "with output :compact"
-      (with-output-style :compact
-        (is (= (render-css declaration)
-               "foo: bar"))
-        (is (= (render-css declaration')
-               "foo-bar-baz: quux"))
-        (is (= (render-css rule)
-               "foo { bar: baz; quux: grault; }"))))
-    (testing "with output :expanded"
-      (with-output-style :expanded
-        (is (= (render-css declaration)
-               "  foo: bar"))
-        (is (= (render-css declaration')
-               "  foo-bar-baz: quux"))
-        (is (= (render-css rule)
-               "foo {\n  bar: baz;\n  quux: grault;\n}"))
-        (is (= (render-css stylesheet)
-               "foo {\n  bar: baz;\n}\n\nquux {\n  corge: grault;\n}"))))
-    (testing "with output :invalid"
-      (with-output-style :invalid
-        (is (= (render-css declaration)
-               "foo:bar"))
-        (is (= (render-css declaration')
-               "foo-bar-baz:quux"))
-        (is (= (render-css rule)
-               "foo{bar:baz;quux:grault}"))
-        (is (= (render-css stylesheet)
-               "foo{bar:baz}quux{corge:grault}"))))
-    (testing "media queries"
-      (is (= (compile-css ^:screen [:h1 {:a "b"}])
-             "@media screen{h1{a:b}}"))
-      (is (= (compile-css ^:screen [[:h1 {:c "d"}] [:h2 {:e "f"}]]
-                          [:h1 {:a "b"}])
-             "h1{a:b}@media screen{h1{c:d}h2{e:f}}"))
-      (is (= (compile-css [:h1 {:a "b"} ^:screen [:&:hover {:c "d"}]])
-             "h1{a:b}@media screen{h1:hover{c:d}}"))
-      (is (= (compile-css ^:toast [:h1 {:a "b"}])
-             "h1{a:b}")))))
+  (testing "with output :compressed"
+    (is (= (render-css {:foo "bar"})
+           "foo:bar"))
+    (is (= (render-css {:foo {:bar {:baz "quux"}}})
+           "foo-bar-baz:quux"))
+    (is (= (render-css [:foo {:bar "baz"} {:quux "grault"}])
+           "foo{bar:baz;quux:grault}"))
+    (is (= (render-css [:foo {:bar "baz"} [:quux {:corge "grault"}]])
+           "foo{bar:baz}foo quux{corge:grault}"))
+    (is (= (render-css [:foo :bar {:bar "baz"} [:quux :corge {:corge "grault"}]])
+           "foo,bar{bar:baz}foo quux,foo corge,bar quux,bar corge{corge:grault}"))
+    (is (= (render-css [:foo :bar '({:bar "baz"}) '([:quux :corge {:corge "grault"}])])
+           "foo,bar{bar:baz}foo quux,foo corge,bar quux,bar corge{corge:grault}"))
+    (is (= (render-css [[:foo {:bar "baz"}] [:quux {:corge "grault"}]])
+           "foo{bar:baz}quux{corge:grault}")))
+  
+  (testing "media queries"
+    (is (= (compile-css ^:screen [:h1 {:a "b"}])
+           "@media screen{h1{a:b}}"))
+    (is (= (compile-css ^:screen [[:h1 {:c "d"}] [:h2 {:e "f"}]]
+                        [:h1 {:a "b"}])
+           "h1{a:b}@media screen{h1{c:d}h2{e:f}}"))
+    (is (= (compile-css [:h1 {:a "b"} ^:screen [:&:hover {:c "d"}]])
+           "h1{a:b}@media screen{h1:hover{c:d}}"))
+    (is (= (compile-css ^:toast [:h1 {:a "b"}])
+           "h1{a:b}"))))
 
--- test/garden/core_test.clj
+++ test/garden/core_test.clj
@@ -1,2 +1,55 @@
 (ns garden.core-test
-  (:use clojure.test))
+  (:use clojure.test
+        garden.core)
+  (:require [garden.util :refer [with-output-style]]))
+
+(defn output [f]
+  (slurp (format "test/garden/output/%s.css" f)))
+
+(defn compressed [s]
+  (output (str "compressed-" (name s))))
+
+(defn compact [s]
+  (output (str "compact-" (name s))))
+
+(defn expanded [s]
+  (output (str "expanded-" (name s))))
+
+(deftest test-css
+  (let [s1 [:h1 {:font-weight "bold"}]
+        s1' [:h1 {:font {:weight "bold"}}]
+        s1'' (for [i (range 1 2)]
+               [(str "h" i) {:font-weight "bold"}])
+        s2 [:h1 {:font-weight "bold"}
+            [:a {:text-decoration "none"}]]
+        s3 [:a
+            {:font-weight "normal"}
+            [:&:hover {:font-weight "bold"}]]
+        s4 (vary-meta (list s1 s3) assoc :screen true)]
+
+    (testing "compressed output"
+      (with-output-style :compressed
+        (is (= (css s1) (compressed :s1)))
+        (is (= (css s1') (compressed :s1)))
+        (is (= (css s1'') (compressed :s1)))
+        (is (= (css s2) (compressed :s2)))
+        (is (= (css s3) (compressed :s3)))
+        (is (= (css s4) (compressed :s4)))))
+
+    (testing "compact output"
+      (with-output-style :compact
+        (is (= (css s1) (compact :s1)))
+        (is (= (css s1') (compact :s1)))
+        (is (= (css s1'') (compact :s1)))
+        (is (= (css s2) (compact :s2)))
+        (is (= (css s3) (compact :s3)))
+        (is (= (css s4) (compact :s4)))))
+
+    (testing "exapnded output"
+      (with-output-style :expanded
+        (is (= (css s1) (expanded :s1)))
+        (is (= (css s1') (expanded :s1)))
+        (is (= (css s1'') (expanded :s1)))
+        (is (= (css s2) (expanded :s2)))
+        (is (= (css s3) (expanded :s3)))
+        (is (= (css s4) (expanded :s4)))))))
