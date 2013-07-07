e494d9cf0d4224ef4f85419342a4b115808a662f
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-07-07 01:06:47 -0700

    Clean up and document psuedo-classes ns

--- src/garden/stylesheet/pseudo_classes.clj
+++ src/garden/stylesheet/pseudo_classes.clj
@@ -1,7 +1,8 @@
 (ns garden.stylesheet.pseudo-classes
+  "Functions for defining styles for pseudo classes."
   (:refer-clojure :exclude [empty])
-  (:require [garden.util :as util]
-            [garden.def :refer [rule defrule]]))
+  (:require [garden.util :as u]
+            [garden.def :refer [defrule]]))
 
 (doseq [c '[active focus visited hover link
             enabled disabled checked
@@ -10,43 +11,45 @@
             empty root
             required optional valid invalid
             target]]
-  (eval `(defrule ~c ~(str "&:" c))))
+  (let [doc (str "Define styles for the :" c " pseudo class.")]
+    (eval `(defrule ~(with-meta c {:doc doc})
+             ~(str "&:" c)))))
 
-(defrule ^{:doc "CSS3 ::before pseudo selector"}
-  before "&::before")
+;; CSS3 introduced the new double colon syntax (ie. `::before`) to
+;; distinguish the difference between pseudo-classes and
+;; pseudo-elements. Browsers accept both notations so why not kill two
+;; birds with one stone?
 
-(defrule ^{:doc "CSS3/CSS2 :before pseudo selector."}
-  before' "&::before" "&:before")
+(doseq [c '[before after first-line first-letter]]
+  (let [doc (str "Define styles for the CSS3 ::" c
+                 " and CSS2 :" c" pseudo class.")]
+    (eval `(defrule ~(with-meta c {:doc doc})
+             ~(str "&::" c) ~(str "&:" c)))))
 
-(defrule ^{:doc "CSS3 ::after pseudo selector"}
-  after "&::after")
-
-(defrule ^{:doc "CSS3/CSS2 :after pseudo selector."}
-  after' "&::after" "&:after")
-
-(defrule ^{:doc "CSS3 ::first-line pseudo selector"}
-  first-line "&::first-line")
-
-(defrule ^{:doc "CSS3/CSS2 :first-line pseudo selector."}
-  first-line' "&::first-line" "&:first-line")
-
-(defrule ^{:doc "CSS3 ::first-letter pseudo selector"}
-  first-letter "&::first-letter")
-
-(defrule ^{:doc "CSS3/CSS2 :first-letter pseudo selector."}
-  first-letter' "&::first-letter" "&:first-letter")
-
-(defrule ^{:doc "::selection pseudo selector."}
+(defrule ^{:doc "Define styles for the ::selection and ::-moz-selection
+                 pseudo class."}
   selection "&::selection" "&::-moz-selection")
 
 (defn lang
+  "Define styles for the :lang pseudo class.
+
+   ex. (lang :fr {:quotes [\"«\" \"»\"]})
+       => [\"&:lang(fr) {:quotes [\"«\" \"»\"]}]"
   [l & children]
-  [(format "&:lang(%s)" (util/to-str l)) children])
+  [(format "&:lang(%s)" (u/to-str l)) children])
 
 (defn nth-child
+  "Define styles for the :nth-child psuedo class.
+
+   ex. (nth-child :even {:background \"green\"})
+       => [\"&:nth-child(even)\" {:background \"green\"} "
   [n & children]
-  [(format "&:nth-child(%s)" (util/to-str n)) children])
+  [(format "&:nth-child(%s)" (u/to-str n)) children])
 
 (defn nth-of-type
+  "Define styles for the :nth-of-type psuedo class.
+
+   ex. (nth-of-type \"2n+1\" {:font-weight \"bold\"})
+       => [\"&:nth-of-type(2n+1)\" {:font-weight \"bold\"} "
   [n & children]
-  [(format "&:nth-of-type(%s)" (util/to-str n)) children])
+  [(format "&:nth-of-type(%s)" (u/to-str n)) children])
