7937b936828bc2e9066a5faf1e03c72cca8fb815
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-07-07 22:00:37 -0700

    Add codox, improve project description

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject garden "0.1.0-beta6"
-  :description "A library for generating CSS in Clojure."
+  :description "Generating CSS from Clojure data structures."
   :url "https://github.com/noprompt/garden"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.combinatorics "0.0.4"]]
-  :profiles {:dev {:source-paths ["dev"]}})
+  :profiles {:dev {:source-paths ["dev"]}}
+  :plugins [[codox "0.6.4"]])
