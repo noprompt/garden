73d308c6f0087cfac472550d3e92938130cd9055
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-04 02:23:16 -0700

    Spruce up user ns

--- dev/user.clj
+++ dev/user.clj
@@ -1,17 +1,18 @@
 (ns user
   (:refer-clojure :exclude [+ - * /])
   (:require [garden.core :refer [css]]
-            [garden.util :as u]
-            [garden.units :as un]
-            [garden.color :as c]
-            [garden.def :refer [defrule]]
+            [garden.util :as util]
+            [garden.units :as units :refer [px]]
+            [garden.color :as color]
+            [garden.def :refer [defrule defcssfn]]
+            [garden.stylesheet :as stylesheet]
             ;; Comment this line before running tests to prevent
             ;; warning messages. 
             ;;[garden.arithmetic :refer [+ - * /]]
-            [clojure.string :as s]
+            [clojure.string :as string]
             [clojure.pprint :refer [pprint pp]]
             [clojure.repl :refer [source doc]]))
 
 (defn debug [x]
-  (clojure.pprint/pprint x)
+  (pprint x)
   x)
