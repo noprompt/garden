2ffd1f40530258d1b4bcc7d04dd190a6fcc8f68a
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-11 01:25:19 -0700

    Use docstrings for CSSFunctions

--- src/garden/stylesheet/functions.clj
+++ src/garden/stylesheet/functions.clj
@@ -2,18 +2,20 @@
   (:require [garden.def :refer [defcssfn]]))
 
 (defcssfn url
-  ;; Create a CSS `url` function.
+  "Create a CSS `url` function."
   [url]
   url)
 
 (defcssfn attr
-  ;; Create a CSS attr function.
+  "Create a CSS attr function."
   ([name] name)
-  ([name type-or-unit] [[name type-or-unit]])
-  ([name type-or-unit fallback] [name [type-or-unit fallback]]))
+  ([name type-or-unit]
+     [[name type-or-unit]])
+  ([name type-or-unit fallback]
+     [name [type-or-unit fallback]]))
 
 (defcssfn toggle
-  ;; Create CSS toggle function.
+  "Create CSS toggle function."
   ([value] value)
   ([value & more] (cons value more)))
 
