b227225caea2ae0baa5396238734830aed047961
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-14 18:49:43 -0700

    Create a new ns garden.repl
    This namespace includes definitions for print-methods.

---
+++ src/garden/repl.clj
@@ -0,0 +1,27 @@
+(ns garden.repl
+  (:require [garden.util :as util :refer [ToString to-str]])
+  (:import garden.types.CSSUnit
+           garden.types.CSSFunction))
+
+(extend-protocol ToString
+  CSSUnit
+  (to-str [{:keys [magnitude unit]}]
+    (let [magnitude (if (ratio? magnitude)
+                    (float magnitude)
+                    magnitude)]
+    (str (if (zero? magnitude) 0 magnitude)
+         (when-not (zero? magnitude) (name unit)))))
+
+  CSSFunction
+  (to-str [{:keys [function args]}]
+    (format "%s(%s)"
+            (util/to-str function)
+            (if (sequential? args)
+              (util/comma-join args)
+              args))))
+
+(defmethod print-method CSSUnit [unit writer]
+  (.write writer (to-str unit)))
+
+(defmethod print-method CSSFunction [cssfn writer]
+  (.write writer (to-str cssfn)))
