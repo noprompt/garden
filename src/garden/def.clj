a40ab5ec0b6eff698702641fb2f15f5cac2849b0
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-16 23:31:24 -0700

    Document defcssfn clean up docstrings

--- src/garden/def.clj
+++ src/garden/def.clj
@@ -3,17 +3,17 @@
 
 (defn rule
   "Create a rule function for the given selector. The `selector`
-   argument must be valid selector (ie. a keyword, string, or symbol).
-   Additional arguments may consist of extra selectors or
-   declarations.
+  argument must be valid selector (ie. a keyword, string, or symbol).
+  Additional arguments may consist of extra selectors or
+  declarations.
 
-   The returned function accepts any number of arguments which represent
-   the rule's children.
+  The returned function accepts any number of arguments which represent
+  the rule's children.
 
-   Ex.
-       (let [text-field (rule \"[type=\"text\"])]
-        (text-field {:border [\"1px\" :solid \"black\"]}))
-       => [\"[type=\"text\"] {:boder [\"1px\" :solid \"black\"]}]"
+  Ex.
+      (let [text-field (rule \"[type=\"text\"])]
+       (text-field {:border [\"1px\" :solid \"black\"]}))
+      ;; => [\"[type=\"text\"] {:boder [\"1px\" :solid \"black\"]}]"
   [selector & more]
   (if-not (or (keyword? selector)
               (string? selector)
@@ -25,20 +25,22 @@
 
 (defmacro defrule
   "Define a function for creating rules. If only the `name` argument is
-   provided the rule generating function will default to using it as the
-   primary selector.
+  provided the rule generating function will default to using it as the
+  primary selector.
 
-   Ex.
-       (defrule a)
-       => #'user/a
-       (a {:text-decoration \"none\"})
-       => [:a {:text-decoration \"none\"}]
+  Ex.
+      (defrule a)
+      ;; => #'user/a
 
-   Ex.
-       (defrule sub-headings :h4 :h5 :h6)
-       => #'user/sub-headings
-       (sub-headings {:font-weight \"normal\"})
-       => [:h4 :h5 :h6 {:font-weight \"normal\"}]"
+      (a {:text-decoration \"none\"})
+      ;; => [:a {:text-decoration \"none\"}]
+
+  Ex.
+      (defrule sub-headings :h4 :h5 :h6)
+      ;; => #'user/sub-headings
+
+      (sub-headings {:font-weight \"normal\"})
+      ;; => [:h4 :h5 :h6 {:font-weight \"normal\"}]"
   [name & selectors]
   (let [rfn (if (seq selectors)
               (apply rule selectors)
@@ -51,7 +53,47 @@
   (fn [& args]
     (CSSFunction. fn-name args)))
 
-(defmacro defcssfn
+(defmacro ^{:arglists '([name] [name docstring? & fn-tail])}
+  defcssfn
+  "Define a function for creating custom CSS functions. The generated
+  function will automatically create an instance of
+  `garden.types.CSSFunction` of which the `:args` field will be set
+  to whatever the return value of the original function is. The
+  `:function` field will be set to `(str name)`.
+
+  If only the `name` argument is provided the returned function will
+  accept any number of arguments.
+
+  Ex.
+      (def cssfn url)
+      ;; => #'user/url
+
+      (url \"http://fonts.googleapis.com/css?family=Lato\")
+      ;; => #garden.types.CSSFunction{:function \"url\", :args \"http://fonts.googleapis.com/css?family=Lato\"}
+
+      (css (url \"http://fonts.googleapis.com/css?family=Lato\"))
+      ;; => url(http://fonts.googleapis.com/css?family=Lato) 
+
+  Ex.
+      (defcssfn attr
+        ([name] name)
+        ([name type-or-unit]
+           [[name type-or-unit]])
+        ([name type-or-unit fallback]
+           [name [type-or-unit fallback]]))
+      ;; => #'user/attr
+
+      (attr :vertical :length)
+      ;; => #garden.types.CSSFunction{:function \"url\", :args [:vertical :length]}
+
+      (css (attr :vertical :length))
+      ;; => \"attr(vertical length)\"
+
+      (attr :end-of-quote :string :inherit) 
+      ;; => #garden.types.CSSFunction{:function \"url\", :args [:end-of-quote [:string :inherit]]}
+
+      (css (attr :end-of-quote :string :inherit))
+      ;; => \"attr(end-of-quote string, inherit)\""
   ([name]
      (let [name (vary-meta name assoc :arglists '(list '[& args]))]
        `(def ~name (cssfn (str '~name)))))
