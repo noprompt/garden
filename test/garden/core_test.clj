e6f471942167213df1dfacc0dab08c735ccca351
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-19 17:19:22 -0700

    Remove unecissary core tests
    All output testing is handled in compiler_test. There's not really a
    good reason to double check.

--- test/garden/core_test.clj
+++ test/garden/core_test.clj
@@ -1,32 +1,3 @@
 (ns garden.core-test
   (:use clojure.test
         garden.core))
-
-(defn output [f]
-  (slurp (format "test/garden/output/%s.css" f)))
-
-(defn expanded [s]
-  (output (str "expanded-" (name s))))
-
-(def css-pretty-print
-  (partial css {:pretty-print? true}))
-
-(deftest test-css
-  (let [s1 [:h1 {:font-weight "bold"}]
-        s1' [:h1 {:font {:weight "bold"}}]
-        s1'' (for [i (range 1 2)]
-               [(str "h" i) {:font-weight "bold"}])
-        s2 [:h1 {:font-weight "bold"}
-            [:a {:text-decoration "none"}]]
-        s3 [:a
-            {:font-weight "normal"}
-            [:&:hover {:font-weight "bold"}]]
-        s4 (vary-meta (list s1 s3) assoc :media {:screen true})]
-
-    (testing "pretty-print output"
-      (is (= (css-pretty-print s1) (expanded :s1)))
-      (is (= (css-pretty-print s1') (expanded :s1)))
-      (is (= (css-pretty-print s1'') (expanded :s1)))
-      (is (= (css-pretty-print s2) (expanded :s2)))
-      (is (= (css-pretty-print s3) (expanded :s3)))
-      (is (= (css-pretty-print s4) (expanded :s4))))))
