380ff419c8a34b7ab3f682578f6c1196a12d833b
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-09-22 01:06:10 -0700

    Add tests for defcssfn and defkeyframes

--- test/garden/def_test.clj
+++ test/garden/def_test.clj
@@ -1,6 +1,8 @@
 (ns garden.def-test
   (:use clojure.test
-        garden.def))
+        garden.def)
+  (:import (garden.types CSSFunction
+                         CSSAtRule)))
 
 (defrule a)
 (defrule sub-headings :h4 :h5 :h6)
@@ -19,3 +21,24 @@
            [:a {:font-weight "bold"}]))
     (is (= (sub-headings {:font-weight "normal"})
            [:h4 :h5 :h6 {:font-weight "normal"}]))))
+
+(defcssfn bar)
+
+(defcssfn foo
+  ([x] x)
+  ([x y] [x y]))
+
+(deftest defcssfn-test
+  (is (instance? CSSFunction (bar 1)))
+  (is (= '(1) (:args (bar 1))))
+  (is (= 1 (:args (foo 1))))
+  (is (= [1 2] (:args (foo 1 2)))))
+
+(defkeyframes fade-out
+  [:from {:opacity 1}]
+  [:to {:opacity 0}])
+
+(deftest defkeyframes-test
+  (is (instance? CSSAtRule fade-out))
+  (is (= :keyframes (:identifier fade-out)))
+  (is (= "fade-out" (get-in fade-out [:value :identifier]))))
