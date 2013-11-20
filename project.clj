b8cbecbfcbbd69ebd2dd1d9d9b728c7be6755749
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-11-19 16:25:54 -0800

    Version bump to 1.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject garden "1.1.4-SNAPSHOT"
+(defproject garden "1.1.4"
   :description "Generate CSS from Clojure data structures."
   :url "https://github.com/noprompt/garden"
   :license {:name "Eclipse Public License"
