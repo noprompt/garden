f0949f3c826acfc401adbc2abb5d362b3513a7aa
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-05-23 22:45:29 -0700

    Clean/update project file

--- project.clj
+++ project.clj
@@ -3,26 +3,35 @@
   :url "https://github.com/noprompt/garden"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clj" "target/generated-src/clj" "target/generated-src/cljs"]
-  :test-paths ["test" "target/generated-test"]
+
+  :source-paths
+  ["src/clj" "target/generated-src/clj" "target/generated-src/cljs"]
+
+  :test-paths
+  ["test" "target/generated-test"]
 
   :dependencies
-  [[org.clojure/clojure "1.5.1"]
+  [[org.clojure/clojure "1.6.0"]
    [com.yahoo.platform.yui/yuicompressor "2.4.7"]]
 
   :profiles
-  {:dev {:dependencies [[criterium "0.4.1"]
-                        [org.clojure/clojurescript "0.0-2138"]
-                        [com.cemerick/piggieback "0.1.2"]]
-         :plugins [[com.cemerick/austin "0.1.3"]
-                   [codox "0.6.4"]
-                   [lein-cljsbuild "1.0.0"]
-                   [org.clojure/clojure "1.6.0"]
-                   [com.keminglabs/cljx "0.3.2"]
-                   [com.cemerick/clojurescript.test "0.2.1"]]
-         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+  {:dev {:dependencies
+         [[criterium "0.4.1"]
+          [org.clojure/clojurescript "0.0-2197"]
+          [com.cemerick/piggieback "0.1.2"]]
 
-  :aliases {"test-all" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]}
+         :plugins
+         [[com.cemerick/austin "0.1.3"]
+          [codox "0.6.4"]
+          [lein-cljsbuild "1.0.3"]
+          [com.keminglabs/cljx "0.3.2"]
+          [com.cemerick/clojurescript.test "0.3.1"]]
+
+         :repl-options
+         {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+
+  :aliases
+  {"test-all" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]}
 
   :cljx
   {:builds [{:source-paths ["src/cljx"]
@@ -39,10 +48,15 @@
              :rules :cljs}]}
 
   :cljsbuild
-  {:builds [{:source-paths ["target/generated-src/cljs" "target/generated-test"]
-             :compiler {:output-to "target/cljs/testable.js"
-                        :optimizations :whitespace
-                        :pretty-print true}}]
-   :test-commands {"unit-tests" ["phantomjs" :runner "target/cljs/testable.js"]}}
+  {:builds
+   [{:source-paths ["target/generated-src/cljs" "target/generated-test"]
+     :notify-command ["phantomjs" :cljs.test/runner
+                      "target/cljs/testable.js"]
+     :compiler {:output-to "target/cljs/testable.js"
+                :optimizations :whitespace
+                :pretty-print true}}]
+   
+   :test-commands
+   {"unit-tests" ["phantomjs" :runner "target/cljs/testable.js"]}}
 
   :global-vars {*warn-on-reflection* true})
