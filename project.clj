e78eed6b9e2c42b6960a03149c62541521b254b5
Joel Holdbrooks <cjholdbrooks@gmail.com>
2017-09-25 10:41:19 -0700

    Set development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject garden "1.3.3"
+(defproject garden "1.3.4-SNAPSHOT"
   :description "Generate CSS from Clojure/Cljs data structures."
   :url "https://github.com/noprompt/garden"
   :license {:name "Eclipse Public License"
