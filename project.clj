e6a7ca930c2de84a93373c19b4d2eb212f906a48
Joel Holdbrooks <cjholdbrooks@gmail.com>
2019-04-10 15:15:57 -0700

    Version bump to 1.3.8

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject garden "1.3.7-SNAPSHOT"
+(defproject garden "1.3.8"
   :description "Generate CSS from Clojure(Script) data structures."
   :url "https://github.com/noprompt/garden"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.5.0"
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8"
                   :exclusions [rhino/js]]]
